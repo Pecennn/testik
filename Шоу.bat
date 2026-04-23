f2ab16a782b348879c6e4ab7cae6652ef1862f0e
Firefint <163530015+Pecennn@users.noreply.github.com>
2026-04-23 23:06:30 +0300

    Update Шоу.bat

--- Шоу.bat
+++ Шоу.bat
@@ -3,9 +3,9 @@ chcp 65001 >nul
 
 :: --- НАСТРОЙКИ ---
 set "SITE_URL=https://www.youtube.com/watch?v=5JpA5SVVpac"
-set "MUSIC_URLhttps://raw.githubusercontent.com/Pecennn/testik/refs/heads/main/sex.mp4"
+set "MUSIC_URL=https://raw.githubusercontent.com/Pecennn/testik/refs/heads/main/sex.mp4"
 set "BASE_RAW=https://raw.githubusercontent.com/Pecennn/testik/main"
-set "PHOTOS=a4.png kani.png lenin.png lev.png mge.png noshka.png shrek.png svin.png negritos.png kon.png pchelkon.png iaico.png polynichka.png"
+set "PHOTOS=a4.png kani.png lenin.png lev.png mge.png noshka.png shrek.png svin.png negritos.png kon.png pchelkon.png iaico.png polynichka.png pobeg.gif"
 set "WALL_URL=https://raw.githubusercontent.com/Pecennn/testik/main/%%D0%%BA%%D0%%BE%%D0%%BD%%D1%%8C.png"
 
 :: [1/3] Загрузка ресурсов (в скрытом режиме)
